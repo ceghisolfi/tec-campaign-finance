4634264653a0cc777e17bc46d52362f606017b11
Caroline Ghisolfi <cghisolfi@gannett.com>
2022-04-15 03:05:18 -0500

    update app:

--- analysis/campaign-finance-app.py
+++ analysis/campaign-finance-app.py
@@ -6,8 +6,10 @@ from zipfile import ZipFile
 from csv import reader
 import altair as alt
 import streamlit as st
-
-
+import warnings
+from urllib import request
+warnings.filterwarnings('ignore')
+import time
 
 # Page settings
 st.set_page_config(
@@ -53,14 +55,10 @@ text-align: center;
 """
 
 
-
-
 st.title('Campaign Finance Data Tool')
 
-# with open('https://data-statesman.s3.amazonaws.com/tec-campaign-finance/documentation/last_update.txt') as f:
-#     last_update = f.read()
-
-last_update = 'Apr 14, 2022'
+with request.urlopen('https://data-statesman.s3.amazonaws.com/tec-campaign-finance/documentation/last_update.txt') as f:
+    last_update = f.read().decode('utf-8')
 
 st.markdown(f'##### Last update: {last_update}')
 st.markdown("""
@@ -73,12 +71,12 @@ To begin, select one or more filers by **TYPE** and **NAME**.
 
 """)
 
-#------------ FUNCTIONS ----------------
 
 @st.cache
 def convert_df(df):
      # IMPORTANT: Cache the conversion to prevent computation on every rerun
      return df.to_csv().encode('utf-8')
+
 
 @st.cache
 def load_filers():
@@ -90,7 +88,12 @@ def load_filers():
     return filers
 
 
-def filter_data(filername, var, prefix, var_short):
+def filter_data(filername, dtype):
+
+    # Load dtype vars
+    var, prefix, var_short = dtype[0], dtype[1], dtype[2]
+
+    # Filter data
     filer_initials = filername[:3]
     if var == 'Expenditure':
         vardt = 'expend'
@@ -110,31 +113,54 @@ def filter_data(filername, var, prefix, var_short):
         return []
 
 
-
-def get_stats(monthly, filtered_data, this_year, var, prefix):
-
-    avg_amount_this_year = round(monthly[monthly.year == this_year]['amount'].mean())
-    avg_amount_last_year = round(monthly[monthly.year == this_year - 1]['amount'].mean())
-    avg_amount_diff = round(avg_amount_this_year - avg_amount_last_year)
-
-    avg_count_this_year = round(monthly[monthly.year == this_year]['count'].mean())
-    avg_count_last_year = round(monthly[monthly.year == this_year - 1]['count'].mean())
-    avg_count_diff = round(avg_count_this_year - avg_count_last_year)
-
-    top_contrib_this_year = filtered_data[filtered_data.year == this_year].sort_values(f'{var} Amount', ascending=False).iloc[0]
-    top_contrib_this_year_name = top_contrib_this_year[f'{prefix} Name']
-    top_contrib_this_year_info = 'Located at zipcode ' + str(top_contrib_this_year[f'{prefix} Street Postal Code']) + ' in ' + str(top_contrib_this_year[f'{prefix} Street City']) + ', ' + str(top_contrib_this_year[f'{prefix} Street State'].upper())
-    try:
-        if top_contrib_this_year[f'{prefix} Employer'] != '':
-            top_contrib_this_year_info = 'Employed by ' + str(top_contrib_this_year[f'{prefix} Employer']) + ' in ' + str(top_contrib_this_year[f'{prefix} Street City']) + ', ' + str(top_contrib_this_year[f'{prefix} Street State'].upper())
-    except:
-        pass
-
-    return avg_amount_this_year, avg_amount_diff, avg_count_this_year, avg_count_diff, top_contrib_this_year_name, top_contrib_this_year_info
+def display_filertable(filertable):
+    with st.expander('Filer Information'):
+            st.dataframe(filertable.fillna(''))
 
 
+def display_stats(dtype):
 
-def display_data(filtered_data, var, returndf, filername):
+    # Load dtype vars
+    var, prefix, varshort, filtered_data = dtype[0], dtype[1], dtype[2], dtype[3]
+
+    if len(filtered_data) > 0:
+
+        # Get stats
+        this_year = filtered_data.year.max()
+
+        if this_year - 1 in filtered_data.year.unique():
+            monthly = filtered_data.groupby([pd.Grouper(key=f'{var} Dt', freq='M'), 'year'])\
+                            .agg(amount = (f'{var} Amount', 'sum'), count = (f'{var} Amount', 'count')).reset_index().fillna(0)
+
+            avg_amount_this_year = round(monthly[monthly.year == this_year]['amount'].mean())
+            avg_amount_last_year = round(monthly[monthly.year == this_year - 1]['amount'].mean())
+            avg_amount_diff = round(avg_amount_this_year - avg_amount_last_year)
+
+            avg_count_this_year = round(monthly[monthly.year == this_year]['count'].mean())
+            avg_count_last_year = round(monthly[monthly.year == this_year - 1]['count'].mean())
+            avg_count_diff = round(avg_count_this_year - avg_count_last_year)
+
+            top_contrib_this_year = filtered_data[filtered_data.year == this_year].sort_values(f'{var} Amount', ascending=False).iloc[0]
+            top_contrib_this_year_name = top_contrib_this_year[f'{prefix} Name']
+            top_contrib_this_year_info = 'Located at zipcode ' + str(top_contrib_this_year[f'{prefix} Street Postal Code']) + ' in ' + str(top_contrib_this_year[f'{prefix} Street City']) + ', ' + str(top_contrib_this_year[f'{prefix} Street State'].upper())
+            try:
+                if top_contrib_this_year[f'{prefix} Employer'] != '':
+                    top_contrib_this_year_info = 'Employed by ' + str(top_contrib_this_year[f'{prefix} Employer']) + ' in ' + str(top_contrib_this_year[f'{prefix} Street City']) + ', ' + str(top_contrib_this_year[f'{prefix} Street State'].upper())
+            except:
+                pass
+
+            # Display stats
+            col1, col2, col3 = st.columns(3)
+            col1.metric(label=f"Avg Monthly {var} Amount ({this_year})", value='${:,}'.format(avg_amount_this_year), delta='${:,}'.format(avg_amount_diff))
+            col2.metric(label=f"Avg Monthly {var} Count ({this_year})", value='{:,}'.format(avg_count_this_year), delta='{:,}'.format(avg_count_diff))
+            col3.metric(label=f'Top {prefix} ({this_year})', value=top_contrib_this_year_name, delta=top_contrib_this_year_info, delta_color='off')
+
+
+def display_data(dtype, filername):
+
+    # Load dtype vars
+    var, prefix, var_short, filtered_data = dtype[0], dtype[1], dtype[2], dtype[3]
+
     # Display data table
     if len(filtered_data) > 0:
         date_min, date_max = filtered_data[f'{var} Dt'].min().strftime(format='%b %d, %Y'), filtered_data[f'{var} Dt'].max().strftime(format='%b %d, %Y')
@@ -142,7 +168,7 @@ def display_data(filtered_data, var, returndf, filername):
             filtered_data[col] = pd.to_datetime(filtered_data[col]).apply(lambda x: x.strftime(format='%Y-%m-%d'))
         with st.expander(f'{var}s'):
             st.markdown(f'**Date range**: {date_min} - {date_max}')
-            data_csv = convert_df(returndf.drop(columns='year'))
+            data_csv = convert_df(filtered_data.drop(columns='year'))
             st.download_button(
                 label=f"Download {var.lower()} data",
                 data=data_csv,
@@ -151,30 +177,17 @@ def display_data(filtered_data, var, returndf, filername):
             )
             st.dataframe(filtered_data.fillna(''))
 
-            # return returndf
     else:
-        st.write('No data to display')
+        st.write(f'Insufficient {var.lower()} data to display')
 
 
+def get_common(concat_dfs, year, dtype):
 
-def make_chart(concat_dfs, var):
-    grouped = concat_dfs.groupby(['Filer Name', pd.Grouper(key=f'{var} Dt', freq='m')])[f'{var} Amount'].sum().reset_index()
-    grouped['Filer Name'] = grouped['Filer Name'].apply(lambda x: re.sub("([\(\[]).*?([\)\]])", "", x))
-    chart = alt.Chart(grouped).mark_line().encode(
-                                    x=alt.X(f'{var} Dt', axis=alt.Axis(labelAngle=0), title=''), 
-                                      y=alt.Y(f'{var} Amount', title='', ), 
-                                      color=alt.Color('Filer Name', legend=alt.Legend(title='', labelFontSize=15)),
-                                      tooltip=alt.Tooltip(f'{var} Amount',format=",.2f")
-                                     ).properties(width=1000)
-    return chart
-
-
-
-def get_common(concat_dfs, year, var, prefix):
+    var, prefix = dtype[0], dtype[1]
 
     if year != 'All':
         concat_dfs = concat_dfs[concat_dfs.year == year]
-    grouped = concat_dfs.fillna('').groupby([col for col in concat_dfs.columns if 'contributor' in col.lower()]).agg(
+    grouped = concat_dfs.fillna('').groupby([col for col in concat_dfs.columns if prefix.lower() in col.lower()]).agg(
         contrib_amount = (f'{var} Amount', 'sum'), 
         filers_count = ('Filer Name', 'nunique'), 
         Filers = ('Filer Name', lambda x: ', '.join(x.unique()))).reset_index()
@@ -190,132 +203,146 @@ def get_common(concat_dfs, year, var, prefix):
     return common
 
 
+def display_common(common, dtype, names):
 
-def process_data(filername, var, prefix, var_short): # Contribution, Contributor
+    var, prefix = dtype[0], dtype[1]
 
-    filers = load_filers()
-    
-    if filername:
-        with st.spinner('Loading data'):
-            # Filter and format data
-            filtered_data = filter_data(filername, var, prefix, var_short)
-
-            if len(filtered_data) > 0:
-            
-                returndf = filtered_data.copy()
-                this_year = filtered_data.year.max()
-                monthly = filtered_data.groupby([pd.Grouper(key=f'{var} Dt', freq='M'), 'year'])\
-                    .agg(amount = (f'{var} Amount', 'sum'), count = (f'{var} Amount', 'count')).reset_index().fillna(0)
-
-                # st.markdown(f'### {filername}')
-
-                if (this_year - 1) in list(monthly.year):
-
-                    avg_amount_this_year, avg_amount_diff, avg_count_this_year, avg_count_diff, top_contrib_this_year_name, top_contrib_this_year_info = \
-                        get_stats(monthly, filtered_data, this_year, var, prefix)
-
-                    col1, col2, col3 = st.columns(3)
-                    col1.metric(label=f"Avg Monthly {var} Amount ({this_year})", value='${:,}'.format(avg_amount_this_year), delta='${:,}'.format(avg_amount_diff))
-                    col2.metric(label=f"Avg Monthly {var} Count ({this_year})", value='{:,}'.format(avg_count_this_year), delta='{:,}'.format(avg_count_diff))
-                    col3.metric(label=f'Top {prefix} ({this_year})', value=top_contrib_this_year_name, delta=top_contrib_this_year_info, delta_color='off')
-                    
-
-                filtered_data = filtered_data[[col for col in filtered_data.columns if col not in list(filers.columns)]]\
-                    .sort_values(f'{var} Dt', ascending=False).reset_index(drop=True)
-                filtered_data.drop(columns=['year', f'{prefix} Street City', f'{prefix} Street State', f'{prefix} Street Postal Code', f'{prefix} Street Country'], inplace=True)
-
-                # Display data table
-                # returndf = display_data(filtered_data, var, returndf, filername)
-
-                return filtered_data, var, returndf, filername
+    if len(common) > 0:
+        common_csv = convert_df(common)
+        names_forfile = '_'.join(names)
+        st.download_button(
+            label=f"Download shared {prefix.lower()}s data",
+            data=common_csv,
+            file_name=f"shared_{prefix.lower()}_{names_forfile}.csv",
+            mime='text/csv'
+        )
+        st.dataframe(common)
+    else:
+        st.write('No data to display')
 
 
+def make_chart(concat_dfs, var):
 
-def compare_filers(dfs, var, prefix):
-    if len(dfs) > 1:
-        concat_dfs = pd.concat(dfs)
-        names = [name.split(',')[0] for name in list(concat_dfs['Filer Name'].unique())]
-        compare_title = ' vs. '.join(names)
-        st.markdown("""---""")
-        st.markdown('<p style="background-color:  #ff4c4c; text-align: center; color:white; font-size: 20px; width:100%;"><b>FILER COMPARISON</b></p>', unsafe_allow_html=True)
-        st.markdown(footer,unsafe_allow_html=True)
-        st.markdown(f'### {compare_title}')
-
-        years_options =['All']
-        years_options.extend(sorted(concat_dfs.year.unique()))
-        this_year = concat_dfs.year.max()
-        min_year = concat_dfs.year.min()
-        year = 'All'
-        common = get_common(concat_dfs, year, var, prefix)
-
-        # Col calculations
-        common_count = len(common)
-        filers_amount_total = concat_dfs[concat_dfs.year == this_year].groupby('Filer Name')[f'{var} Amount'].sum().reset_index().sort_values(f'{var} Amount', ascending=False)
-        top_filer = filers_amount_total.iloc[0]['Filer Name']
-        top_filer_amount = filers_amount_total.iloc[0][f'{var} Amount']
-        runner_up_amount = filers_amount_total.iloc[1][f'{var} Amount']
-        runner_up_filer = filers_amount_total.iloc[1]['Filer Name']
-        runner_up_pct = (top_filer_amount - runner_up_amount) / top_filer_amount * 100
-
-        col1, col2 = st.columns(2)
-
-        col1.metric(label=f'Top Filer by Annual {var}s ({this_year})', value=top_filer, delta=f'{round(runner_up_pct)}% more than runner-up, {runner_up_filer}')
-        col2.metric(label=f"Shared {prefix}s ({min_year} - {this_year})", value='{:,}'.format(common_count))
-
-        with st.expander(f'Compare {var}s'):
-            st.markdown(f'**{var} Monthly Totals ($)**')
-            st.altair_chart(make_chart(concat_dfs, var), use_container_width=True)
-            st.markdown(f'**Shared {prefix}s**')
-
-            year = st.selectbox('Select a year to view common contributors', years_options)
-            common = get_common(concat_dfs, year, var, prefix)
-            if len(common) > 0:
-                common_csv = convert_df(common)
-                names_forfile = '_'.join(names)
-                st.download_button(
-                    label=f"Download shared {prefix.lower()}s data",
-                    data=common_csv,
-                    file_name=f"shared_{var.lower()}_{names_forfile}.csv",
-                    mime='text/csv'
-                )
-                st.dataframe(common)
-            else:
-                st.write('No data to display')
+    concat_dfs[f'{var} Dt'] = pd.to_datetime(concat_dfs[f'{var} Dt'])
+    grouped = concat_dfs.groupby(['Filer Name', pd.Grouper(key=f'{var} Dt', freq='m')])[f'{var} Amount'].sum().reset_index()
+    grouped['Filer Name'] = grouped['Filer Name'].apply(lambda x: re.sub("([\(\[]).*?([\)\]])", "", x))
+    chart = alt.Chart(grouped).mark_line().encode(
+                                    x=alt.X(f'{var} Dt', axis=alt.Axis(labelAngle=0), title=''), 
+                                      y=alt.Y(f'{var} Amount', title='', ), 
+                                      color=alt.Color('Filer Name', legend=alt.Legend(title='', labelFontSize=15)),
+                                      tooltip=alt.Tooltip(f'{var} Amount',format=",.2f")
+                                     ).properties(width=1000)
+    return chart
 
 
+def display_chart(concat_dfs, dtype):
 
-vardicts = [
-    {'var':'Contribution', 'prefix': 'Contributor', 'var_short': 'contribs'},
-    {'var': 'Expenditure', 'prefix': 'Payee', 'var_short': 'expend'},
-    {'var': 'Loan', 'prefix': 'Lender', 'var_short': 'loan'}
-    ]
+    var, prefix = dtype[0], dtype[1]
+
+    st.markdown(f'**{var} Monthly Totals ($)**')
+    st.altair_chart(make_chart(concat_dfs, var), use_container_width=True)
+    st.markdown(f'**Shared {prefix}s**')
+
+#____________________________________________________________ GET FILER DATA ____________________________________________________________
 
 
-with st.container():
-
+def get_filer_data():
+    # Load filers
     filers = load_filers()
 
+    # Display filters
     filertypeW = st.radio('Filer Type', ('INDIVIDUAL', 'ENTITY'))
     filernameW = st.multiselect(options=list(filers[filers['Filer Persent Type'] == filertypeW]['Filer Name'].unique()), label='Filer Name')
 
+    data = []
 
-    dfs = []
-    for filername in filernameW:   
-        # Display filer table
+    # Display filertable
+    for filername in filernameW:
+
         st.markdown(f'### {filername}')
-        filertable = filers[filers['Filer Name'] == filername].reset_index(drop=True)
-        filertable.dropna(how='all', axis=1, inplace=True)
-        with st.expander('Filer Information'):
-            st.dataframe(filertable.fillna(''))
-        for d in vardicts:
-            filtered_data, var, returndf, filername = process_data(filername, d['var'], d['prefix'], d['var_short'])
-            df = display_data(filtered_data, var, returndf, filername)
-            dfs.append(df)
+
+        # Redefining dtypes
+        contribs=['Contribution', 'Contributor', 'contribs']
+        expend = ['Expenditure', 'Payee', 'expend']
+        loans = ['Loan', 'Lender', 'loans']
+        dtypes = [contribs, expend, loans]
+
+        # Filter data
+        ### *************************ADD LOAD WIDGET
+        for dtype in dtypes:
+            filtered_data = filter_data(filername, dtype)
+            dtype.append(filtered_data) ## dtype format now [var, prefix, varshort, filtered_data]
+                
+        # Display stats for each dtype
+        for dtype in dtypes:
+            if len(dtype[3]) > 0: # filtered_data is dataframe
+                data.append([dtype[0], dtype[3]])
+                display_stats(dtype)
+
+        # Display filertable
+        filertable = filers[filers['Filer Name'] == filername].reset_index(drop=True).dropna(how='all', axis=1)
+        display_filertable(filertable)
+
+        # Display data
+        for dtype in dtypes:
+            display_data(dtype, filername)
+    
+    return filernameW, data
+
+#____________________________________________________________ COMPARE FILERS ____________________________________________________________
+
+# Defining dtypes
+contribs=['Contribution', 'Contributor', 'contribs']
+expend = ['Expenditure', 'Payee', 'expend']
+loans = ['Loan', 'Lender', 'loans']
+dtypes = [contribs, expend, loans]
+
+
+def compare_filers(filernameW, data):
+    if len(filernameW) > 1:
+
+        st.markdown("""---""")
+        st.markdown('<p style="background-color:  #ff4c4c; text-align: center; color:white; font-size: 20px; width:100%;"><b>FILER COMPARISON</b></p>', unsafe_allow_html=True)
+
+        for dtype in dtypes:
+
+            dfs = [el[1] for el in data if el[0] == dtype[0]]
             
-            st.markdown("---")
 
-    for d in vardicts:
-        compare_filers(dfs, d['var'], d['prefix'])
+            if len(dfs) > 0:
+
+                concat_dfs = pd.concat(dfs)
+
+                if len(concat_dfs) >= 10 and concat_dfs['Filer Name'].nunique() > 1:
+
+                # Making main vars
+                    names = [name.split(',')[0] for name in list(concat_dfs['Filer Name'].unique())]
+
+                    # Get years
+                    years_options =['All']
+                    years_options.extend(sorted(concat_dfs.year.unique()))
+
+                    with st.expander(f'Compare {dtype[0]}s'):
+                        # Display chart
+                        display_chart(concat_dfs, dtype)
+
+                        # Display common
+                        common = get_common(concat_dfs, 'All', dtype)
+                        if len(common) > 0:
+                            year = st.selectbox(f'Select a year to view common {dtype[0].lower()}s', years_options)
+                            common = get_common(concat_dfs, year, dtype)
+                            display_common(common, dtype, names)
+                else:
+                    st.write(f'Insufficient data to compare {dtype[0].lower()}s')
+            else:
+                st.write(f'Insufficient data to compare {dtype[0].lower()}s')
 
 
-    st.markdown(footer,unsafe_allow_html=True)
+
+def main():
+
+    filernameW, els = get_filer_data()
+    compare_filers(filernameW, els)
+
+
+main()
