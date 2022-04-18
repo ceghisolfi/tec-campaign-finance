72b825f87a7a463cc7867e966ea94499bb696ae1
Caroline Ghisolfi <cghisolfi@gannett.com>
2022-04-17 19:32:15 -0500

    update app

--- etl/test.py
+++ etl/test.py
@@ -8,6 +8,8 @@ def main():
     # Updating last update txt file
     with open(f'{os.getcwd()}/test.txt', 'w') as f:
         f.write(date.today().strftime(format='%b %d, %Y'))
+    print('SCRIPT IS WORKING!!')
+    print(date.today().strftime(format='%b %d, %Y'))
 
 
 if __name__ == '__main__':
